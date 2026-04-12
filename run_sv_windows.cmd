decda896426753cfaa98fcb7dc0db7a708fe4976
devduck <frederik.kraemer@dev-duck.dev>
2026-04-13 00:35:53 +0200

    Update Windows install script

--- run_sv_windows.cmd
+++ run_sv_windows.cmd
@@ -2,5 +2,8 @@
 echo Installing uv...
 powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 
+sleep 3
+
+SET "PATH=%PATH%;%USERPROFILE%\.local\bin"
 echo Done, starting Signal Viewer...
 powershell -ExecutionPolicy ByPass -NoProfile -Command "Start-Process -WindowStyle Hidden -FilePath 'uv' -ArgumentList 'run sv'"
